0d90e79bad1f09bb598f25008f06f1028ca836ef
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-17 13:01:32 +0530

    Updates

---
+++ precache-manifest.cd0f4a70856579264482bf317dfe5045.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b3e38b75101160129a840b14cfa6ab",
+    "url": "/HangmanGame/index.html"
+  },
+  {
+    "revision": "ff37930bf90676d535f8",
+    "url": "/HangmanGame/static/css/main.34f11ff9.chunk.css"
+  },
+  {
+    "revision": "df99e9b1fd54a34ac580",
+    "url": "/HangmanGame/static/js/2.bfce2e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HangmanGame/static/js/2.bfce2e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff37930bf90676d535f8",
+    "url": "/HangmanGame/static/js/main.ee852d49.chunk.js"
+  },
+  {
+    "revision": "6688436165f654fa62e3",
+    "url": "/HangmanGame/static/js/runtime-main.5965d653.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/HangmanGame/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/HangmanGame/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/HangmanGame/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/HangmanGame/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/HangmanGame/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/HangmanGame/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/HangmanGame/static/media/6.c80394c2.jpg"
+  }
+]);
